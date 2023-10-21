c576453edfa41e7449aa9f9d5894b6e96bb86dcd
Binay Tripathy <binaytripathy123@gmail.com>
2023-10-21 23:50:47 +0530

    Added more functionalities
    Signed-off-by: Binay Tripathy <binaytripathy123@gmail.com>

--- src/Components/Stopwatch.jsx
+++ src/Components/Stopwatch.jsx
@@ -1,39 +1,84 @@
-import React, {useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 const Stopwatch = () => {
-    const [timer, setTimer] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [timer, setTimer] = useState(0);
   const [isActive, setIsActive] = useState(false);
 
 
-    const timerRef = useRef(null);
+  const timerRef = useRef(null);
 
+  const tick = () =>{
+    setTimer((timer) => timer + 1);
+  }
 
-    const startTimer = ()=>{
-        setIsActive(true);
-        timerRef.current = setInterval(() => {
-            setTimer((timer) => timer + 1);
-        }, 1000);
+  const startTimer = () => {
+    setIsActive(true);
+    timerRef.current = setInterval(() => {
+      // if (seconds < 10)
+      //   setSeconds((seconds) => seconds + 1);
+      // else if (seconds >= 10) {
+      //   setMinutes((minutes) => minutes + 1);
+      //   setSeconds(0);
+      // }
+      // console.log(timerRef)
+      tick();
+    }, 1000);
+  }
+
+  const handlePause = () => {
+    setIsActive(false);
+    clearInterval(timerRef.current);
+  };
+  const handleStop = () => {
+    setIsActive(false);
+    clearInterval(timerRef.current);
+    setTimer(0);
+    setMinutes(0);
+    setHours(0);
+  };
+  const handleReset = () => {
+    setTimer(0);
+    setMinutes(0);
+    setHours(0);
+    setIsActive(false);
+    clearInterval(timerRef.current);
+    console.log(timer);
+  };
+
+  const show = () => {
+    if(timer % 10 === 0 && minutes === 3){
+      setTimer(0);
+      setMinutes(0);
+      // setHours(Math.floor(timer / 100));
+      setHours((hours) => hours + 1);
     }
+    else if(timer % 10 === 0){
+      setTimer(0);
+      // setMinutes(Math.floor((timer % 100)/10));
+      setMinutes((minutes) => minutes + 1)
+    }
+  }
+  const timelist = {
+    hours: hours,
+    minutes: minutes,
+    seconds: timer % 10,
+  }
 
-    const handlePause = ()=>{
-        setIsActive(false);
-        clearInterval(timerRef.current);
-    };
-    const handleStop = ()=>{
-        setIsActive(false);
-        clearInterval(timerRef.current);
-        setTimer(0);
-    };
-    
+  useEffect(() => {
+    show();
+  },[]);
+  
+
   return (
     <>
-        <h2>{timer}</h2>
-        {/* <button onClick={startTimer}>Start</button> */}
-        <button onClick={isActive ? handlePause : startTimer}>
+      <h2>{timelist.hours + 'h : ' + timelist.minutes + 'm : ' + timelist.seconds + 's'}</h2>
+      <button onClick={isActive ? handlePause : startTimer}>
         {isActive ? 'Pause' : 'Start'}
       </button>
-        <button onClick={handleStop}>Stop</button>
-        <button onClick={()=>{setTimer(0); console.log(timer);}}>Reset</button>
+      <button onClick={handleStop}>Stop</button>
+      <button onClick={handleReset}>Reset</button>
     </>
   )
 }
