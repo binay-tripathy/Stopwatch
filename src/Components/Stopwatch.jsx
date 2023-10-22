9363a0745c0d7baec287d8fd7f98194f1d320054
Binay Tripathy <binaytripathy123@gmail.com>
2023-10-22 23:47:15 +0530

    Timely commit
    Signed-off-by: Binay Tripathy <binaytripathy123@gmail.com>

--- src/Components/Stopwatch.jsx
+++ src/Components/Stopwatch.jsx
@@ -6,32 +6,39 @@ const Stopwatch = () => {
   const [timer, setTimer] = useState(0);
   const [isActive, setIsActive] = useState(false);
 
-
+  
   const timerRef = useRef(null);
-
-  const tick = () =>{
-    setTimer((timer) => timer + 1);
-  }
+  
+  // const tick = () =>{
+    // setTimer((timer) => timer + 1);
+  // }
+  
+  // useEffect(() => {
+  //   show();
+  // }, [timer])
+  
 
   const startTimer = () => {
     setIsActive(true);
     timerRef.current = setInterval(() => {
       // if (seconds < 10)
-      //   setSeconds((seconds) => seconds + 1);
+        // setSeconds((seconds) => seconds + 1);
       // else if (seconds >= 10) {
-      //   setMinutes((minutes) => minutes + 1);
-      //   setSeconds(0);
-      // }
-      // console.log(timerRef)
-      tick();
-    }, 1000);
-  }
+        //   setMinutes((minutes) => minutes + 1);
+        //   setSeconds(0);
+        // }
+        // console.log(timerRef)
+        // tick();
+    setTimer((timer) => timer + 1);
 
-  const handlePause = () => {
-    setIsActive(false);
-    clearInterval(timerRef.current);
-  };
-  const handleStop = () => {
+      }, 1000);
+    }
+    
+    const handlePause = () => {
+      setIsActive(false);
+      clearInterval(timerRef.current);
+    };
+    const handleStop = () => {
     setIsActive(false);
     clearInterval(timerRef.current);
     setTimer(0);
@@ -46,18 +53,18 @@ const Stopwatch = () => {
     clearInterval(timerRef.current);
     console.log(timer);
   };
-
+  
   const show = () => {
     if(timer % 10 === 0 && minutes === 3){
       setTimer(0);
       setMinutes(0);
-      // setHours(Math.floor(timer / 100));
-      setHours((hours) => hours + 1);
+      setHours(Math.floor(timer / 100));
+      // setHours((hours) => hours + 1);
     }
     else if(timer % 10 === 0){
       setTimer(0);
-      // setMinutes(Math.floor((timer % 100)/10));
-      setMinutes((minutes) => minutes + 1)
+      setMinutes(Math.floor((timer % 100)/10));
+      // setMinutes((minutes) => minutes + 1)
     }
   }
   const timelist = {
@@ -65,12 +72,13 @@ const Stopwatch = () => {
     minutes: minutes,
     seconds: timer % 10,
   }
-
+  
   useEffect(() => {
     show();
-  },[]);
+    console.log(timerRef); 
+  }, [show, timerRef]);
   
-
+  
   return (
     <>
       <h2>{timelist.hours + 'h : ' + timelist.minutes + 'm : ' + timelist.seconds + 's'}</h2>
