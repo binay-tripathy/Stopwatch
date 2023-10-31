462fd073592699a444dd374debff5ff611726bd8
Binay Tripathy <binaytripathy123@gmail.com>
2023-10-31 20:42:15 +0530

    Final commit for the month
    Signed-off-by: Binay Tripathy <binaytripathy123@gmail.com>

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.scss";
+import Stopwatch from "./Components/Stopwatch";
+
+function App() {
+  return (
+    <>
+      <h2>Stopwatch</h2>
+      <Stopwatch />
+    </>
+  );
+}
+
+export default App;
